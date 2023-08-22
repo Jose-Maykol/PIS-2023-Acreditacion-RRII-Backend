7f49ab50227a6623493b059b55fa7f7e356a123d
José Maykol <jpaniura@unsa.edu.pe>
2023-08-22 07:22:20 -0500

    FEATURE: Add folder and evidence migrations

---
+++ database/migrations/2023_08_22_071623_create_evidences_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('evidences', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('path')->nullable();
+            $table->string('file')->nullable();
+            $table->string('type')->nullable();
+            $table->string('size')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('folder_id')->constrained('folders');
+            $table->foreignId('evidenceType_id')->constrained('evidencias_tipo');
+            $table->foreignId('standard_id')->constrained('estandars');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('evidences');
+    }
+};

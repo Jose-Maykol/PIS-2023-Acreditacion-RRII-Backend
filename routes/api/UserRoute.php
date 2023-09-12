8a4d443df2c7e3878fc8b07b00ec0cd42d8eb84f
4rian903 <acanazacua@unsa.edu.pe>
2023-09-12 01:29:56 -0500

    REFACTOR: UserRoute

--- routes/api/UserRoute.php
+++ routes/api/UserRoute.php
@@ -9,7 +9,7 @@ Route::middleware("auth:sanctum")->prefix('users')->group(function () {
 
     //Rutas de Gestion de usarios
     Route::get('profile', [UserController::class, 'userProfile']);
-    Route::put('', [UserController::class, 'updateRoleEstado']);
+    Route::put('', [UserController::class, 'update']);
     Route::post('register', [UserController::class, 'register']);
     Route::get('', [UserController::class, 'listUser']);
     Route::get('enabled_users', [UserController::class, 'listUserHabilitados']);

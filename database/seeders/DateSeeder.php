d0475270fb9f18e44d3e345d7bf48e837bd767c2
Arian Canaza <acanazacua@unsa.edu.pe>
2024-01-03 12:42:51 -0500

    REFACTOR: new DateSeeder

--- database/seeders/DateSeeder.php
+++ database/seeders/DateSeeder.php
@@ -31,6 +31,7 @@ class DateSeeder extends Seeder
         DB::table('date_semesters')->insert([
             'year' => 2024,
             'semester' => 'A',
+            'is_closed' => false,
             'registration_status_id' => RegistrationStatusModel::registrationActiveId(),
         ]);
     }
